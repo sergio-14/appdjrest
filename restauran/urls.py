f75ce5e70a7b4f39e74532c06b8f5b83bfd9512f
sergio <elmatadorsergio1234@gmail.com>
2024-05-06 11:00:13 -0400

    cambios static

--- restauran/urls.py
+++ restauran/urls.py
@@ -30,7 +30,6 @@ urlpatterns = [
     path('signin/', views.signin, name='signin'),
     
     
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
