5c98dbe7330bef834d0c504e4c40ec7fcbbb9a1b
sergio <elmatadorsergio1234@gmail.com>
2024-05-06 09:51:11 -0400

    nuevos cambios diploy

--- menu/views.py
+++ menu/views.py
@@ -10,3 +10,31 @@ from django.contrib.auth import authenticate, login, logout
 #inicio
 def home (request):
     return render(request, 'home.html')
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'POST':
+        # Obtiene el username y la contraseña del formulario
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Autentica al usuario
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # Si el usuario es autenticado correctamente, inicia sesión
+            login(request, user)
+            # Redirige al usuario a algún lugar, por ejemplo, al dashboard
+            return redirect('home')
+        else:
+            # Si la autenticación falla, muestra un mensaje de error
+            error_message = "Usuario y/o contraseña incorrectos."
+            return render(request, 'signin.html', {'error_message': error_message})
+
+    # Si la solicitud no es POST, simplemente renderiza el formulario vacío
+    return render(request, 'signin.html')
+
